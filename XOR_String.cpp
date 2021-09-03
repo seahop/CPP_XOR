c49479273c612fddc828398aad48c9346f3ea534
seahop <sean.hopkins@ironnetcybersecurity.com>
2021-09-02 22:17:41 -0500

    additional output format, optimized slightly

--- XOR_String.cpp
+++ XOR_String.cpp
@@ -1,34 +1,49 @@
 #include <iostream>
-#include <iomanip>
-#include <string>
-using namespace std;
-
 
 int XOR(char* data, int data_len, char* key, int key_len, char* charArray)
 {
 	int counter=0;
+	
 	for (int i = 0; i < data_len; i++) {
 		charArray[i] = 0;
 	}
-	printf("\n");
+	
+	printf("\nOutput Type One: \n");
 	for (int i = 0; i < data_len; i++) {
-		if (i == data_len-1) {
-			data[i] ^= key[i % key_len];
+		if (i == data_len - 1) {
 			charArray[i] += data[i];
-			printf("\\x%x\" };", charArray[i]);
+			data[i] ^= key[i % key_len];
+			printf("\\x%02X\" };", data[i]);
 		}
 		else if (i == 0) {
-			data[i] ^= key[i % key_len];
 			charArray[i] += data[i];
-			printf("char output[] = { \"\\x%x", charArray[i]);
+			data[i] ^= key[i % key_len];
+			printf("char output[] = { \"\\x%02X", data[i]);
 		}
 		else {
-			data[i] ^= key[i % key_len];
 			charArray[i] += data[i];
-			printf("\\x%x", charArray[i]);
+			data[i] ^= key[i % key_len];
+			printf("\\x%02X", data[i]);
 		}
 		counter++;
-	}	
+	}
+
+	printf("\n\nOutput Type Two:\n");
+	for (int i = 0; i < data_len; i++) {
+		if (i == data_len - 1) {
+			charArray[i] ^= key[i % key_len];
+			printf("\"0x%02X\" };", charArray[i]);
+		}
+		else if (i == 0) {
+			charArray[i] ^= key[i % key_len];
+			printf("const char* output[] = { \"0x%02X\", ", charArray[i]);
+		}
+		else {
+			charArray[i] ^= key[i % key_len];
+			printf("\"0x%02X\", ", charArray[i]);
+		}
+	}
+
 	printf("\n");
 	return counter;
 }
@@ -43,7 +58,6 @@ void XOR_Validate(char* data, int counter, char* key, int key_len)
 	printf("\n\nValidating by XORing back to original phrase...\n");
 	for (int i = 0; i < counter; i++) {
 		data[i] ^= key[i % strlen(key)];
-		//charArray[i] += output[i];
 		printf("%c", data[i]);
 	}
 	printf("\n");
@@ -60,10 +74,10 @@ void main()
 	char charArray[1000];
 
 	printf("Enter your XOR key: ");
-	cin.getline(inKey,999);
+	std::cin.getline(inKey,999);
 
 	printf("Enter your word, phrase, etc: ");
-	cin.getline(inData,999);
+	std::cin.getline(inData,999);
 
 	for (int i = 0; i < sizeof(inKey)-1; i++) {
 		key[i] = inKey[i];
